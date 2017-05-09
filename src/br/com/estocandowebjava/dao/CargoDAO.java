54f05a88baeea2ab8f6cba954a441452f427c13c
Suelber Costa <suelbercost@gmail.com>
2017-05-09 04:05:53 -0300

    Criação da classe Setor e atualização da classe Cargo.

--- src/br/com/estocandowebjava/dao/CargoDAO.java
+++ src/br/com/estocandowebjava/dao/CargoDAO.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.mysql.fabric.FabricCommunicationException;
 
@@ -12,15 +13,15 @@ import br.com.estocandowebjava.factoty.ConexaoFactory;
 
 public class CargoDAO {
 
-// DEFINIÇÃO DO COMANDO SQL PARA SALVAR OS DADOS
+	// DEFINIÇÃO DO COMANDO SQL PARA SALVAR OS DADOS
 	public void salvarCargo(Cargo c) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("insert into Cargo (descricao, salario, permissao) values (?, ?, ?) ");
 
-// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
 		Connection conexao = ConexaoFactory.conectar();
 
-// COMANDO DE PREPARAÇÃO
+		// COMANDO DE PREPARAÇÃO
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
 		comando.setString(1, c.getDescricao());
 		comando.setDouble(2, c.getSalario());
@@ -29,15 +30,15 @@ public class CargoDAO {
 		comando.executeUpdate();
 	}
 
-// DEFINIÇÃO DO COMANDO SQL PARA EXCLUIR DADOS
+	// DEFINIÇÃO DO COMANDO SQL PARA EXCLUIR DADOS
 	public void excluirCargo(Cargo c) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("delete from cargo where cod_cargo = ? ");
 
-// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
 		Connection conexao = ConexaoFactory.conectar();
 
-// COMANDO DE PREPARAÇÃO
+		// COMANDO DE PREPARAÇÃO
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
 		comando.setLong(1, c.getCodigo());
 
@@ -45,15 +46,15 @@ public class CargoDAO {
 
 	}
 
-// DEFINIÇÃO DO COMANDO SQL PARA EDITAR OS DADOS
+	// DEFINIÇÃO DO COMANDO SQL PARA EDITAR OS DADOS
 	public void editarCargo(Cargo c) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("update Cargo set descricao = ? where cod_cargo = ? ");
 
-// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
 		Connection conexao = ConexaoFactory.conectar();
 
-// COMANDO DE PREPARAÇÃO
+		// COMANDO DE PREPARAÇÃO
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
 		comando.setString(1, c.getDescricao());
 		comando.setLong(2, c.getCodigo());
@@ -62,15 +63,15 @@ public class CargoDAO {
 
 	}
 
-// DEFINIÇÃO DO COMANDO SQL PARA EDITAR OS DADOS
+	// DEFINIÇÃO DO COMANDO SQL PARA EDITAR OS DADOS
 	public Cargo buscarPorCodigo(Cargo c) throws SQLException {
 		StringBuilder sql = new StringBuilder();
 		sql.append("select cod_cargo, descricao from Cargo where cod_cargo = ? ");
 
-// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
 		Connection conexao = ConexaoFactory.conectar();
 
-// COMANDO DE PREPARAÇÃO
+		// COMANDO DE PREPARAÇÃO
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
 		comando.setLong(1, c.getCodigo());
 
@@ -78,7 +79,7 @@ public class CargoDAO {
 
 		Cargo retorno = null;
 
-// CONDIÇÃO QUE DIZ SE A CONSULTA RETORNOU MAIS DE UM VALOR
+		// CONDIÇÃO QUE DIZ SE A CONSULTA RETORNOU MAIS DE UM VALOR
 		if (resultado.next()) {
 			retorno = new Cargo();
 			retorno.setCodigo(resultado.getLong("cod_cargo"));
@@ -88,79 +89,123 @@ public class CargoDAO {
 		return retorno;
 	}
 
-//CRIAÇÃO DO MÉTODO MAIN
+	//CRIAÇÃO DE ARRAYLIST PARA LISTAR TODOS OS CARGOS
+	public ArrayList<Cargo> listar() throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("select cod_cargo, descricao from Cargo order by salario desc ");
+
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		Connection conexao = ConexaoFactory.conectar();
+
+		// COMANDO DE PREPARAÇÃO
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+
+		ResultSet resultado = comando.executeQuery();
+
+		//CRIAÇÃO DO ARRAYLIST DO TIPO CARGO
+		ArrayList<Cargo> lista = new ArrayList<Cargo>();
+
+		//ESTRUTURA DE REPETIÇÃO PARA COLETAR TODOS OS DADOS DA CONSULTA
+		while (resultado.next()) {
+			Cargo c1 = new Cargo();
+			c1.setCodigo(resultado.getLong("cod_cargo"));
+			c1.setDescricao(resultado.getString("descricao"));
+
+			lista.add(c1);
+		}
+
+		return lista;
+	}
+	
+	public ArrayList<Cargo> buscarPorDescricao(Cargo itemCargo) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("SELECT * FROM Cargo WHERE descricao LIKE ? ");
+		sql.append("ORDER BY descricao ASC ");
+
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+				Connection conexao = ConexaoFactory.conectar();
+
+				// COMANDO DE PREPARAÇÃO
+				PreparedStatement comando = conexao.prepareStatement(sql.toString());
+				comando.setString(1, "%" + itemCargo.getDescricao() + "%");
+				
+				ResultSet resultado = comando.executeQuery();
+				
+				//CRIAÇÃO DO ARRAYLIST DO TIPO CARGO
+				ArrayList<Cargo> lista = new ArrayList<Cargo>();
+
+				//ESTRUTURA DE REPETIÇÃO PARA COLETAR TODOS OS DADOS DA CONSULTA
+				while (resultado.next()) {
+					Cargo c1 = new Cargo();
+					itemCargo.setCodigo(resultado.getLong("cod_cargo"));
+					itemCargo.setDescricao(resultado.getString("descricao"));
+
+					lista.add(c1);
+				}
+
+				return lista;
+				
+	}
+
+	// CRIAÇÃO DO MÉTODO MAIN
 	public static void main(String[] args) {
 		/*
-		 * CÓDIGO INSERÇÃO DOS DADOS NA TABELA 
-		 * Cargo c1 = new Cargo();
-		 * c1.setDescricao("Gestor de Transportes"); 
-		 * c1.setSalario(6000.00);
+		 * CÓDIGO INSERÇÃO DOS DADOS NA TABELA Cargo c1 = new Cargo();
+		 * c1.setDescricao("Gestor de Transportes"); c1.setSalario(6000.00);
 		 * c1.setPermissao(0L);
 		 * 
-		 * Cargo c2 = new Cargo(); 
-		 * c2.setDescricao("Analista de Patrimônio");
-		 * c2.setSalario(1730.00); 
-		 * c2.setPermissao(0L);
+		 * Cargo c2 = new Cargo(); c2.setDescricao("Analista de Patrimônio");
+		 * c2.setSalario(1730.00); c2.setPermissao(0L);
 		 * 
-		 * CargoDAO cdao = new CargoDAO(); 
-		 * // TRATAMENTO DE ERRO DA PERSISTÊNCIA NO BANCO DE DADOS 
-		 * try { 
-		 * 		cdao.salvarCargo(c1); 
-		 * 		cdao.salvarCargo(c2);
-		 * 		System.out.println("O(s) cargo(a) foi(ram) salvo(s) com sucesso."); 
-		 * } catch (SQLException e) {
-		 * 		System.out.println("Ocorreu um erro ao tentar salvar os dados.");
-		 * 		e.printStackTrace(); 
-		 * }
+		 * CargoDAO cdao = new CargoDAO(); // TRATAMENTO DE ERRO DA PERSISTÊNCIA
+		 * NO BANCO DE DADOS try { cdao.salvarCargo(c1); cdao.salvarCargo(c2);
+		 * System.out.println("O(s) cargo(a) foi(ram) salvo(s) com sucesso."); }
+		 * catch (SQLException e) {
+		 * System.out.println("Ocorreu um erro ao tentar salvar os dados.");
+		 * e.printStackTrace(); }
 		 */
 
-		 /*
-		 * CÓDIGO INSERÇÃO DOS DADOS NA TABELA 
-		 * Cargo c1 = new Cargo();
+		/*
+		 * CÓDIGO INSERÇÃO DOS DADOS NA TABELA Cargo c1 = new Cargo();
 		 * c1.setCodigo(3L);
 		 * 
 		 * Cargo c2 = new Cargo(); c2.setCodigo(7L);
 		 * 
 		 * CargoDAO cdao = new CargoDAO();
 		 * 
-		 * // TRATAMENTO DE ERRO DE EXCLUSÃO NO BANCO DE DADOS 
-		 * try {
-		 * 		cdao.excluirCargo(c1); 
-		 * 		cdao.excluirCargo(c2);
-		 * 		System.out.println("Os cargos foram excluidos com sucesso."); 
-		 * } catch (SQLException e) {
-		 *		System.out.println("Ocorreu um erro ao tentar excluir o(s) cargo(s)."); 
-		 *		e.printStackTrace(); 
-		 * } 
-		 */
-		 
-		 /* 
-		 * CÓDIGO ALTERAÇÃO DOS DADOS NA TABELA 
-		 * Cargo c1 = new Cargo();
-		 * c1.setCodigo(2L); 
-		 * c1.setDescricao("Analista de Expedição");
-		 * 
-		 * CargoDAO cdao = new CargoDAO(); 
-		 * 
-		 * // TRATAMENTO DE ERRO DA ALTERAÇÃO NO BANCO DE DADOS
-		 * try { cdao.editarCargo(c1);
-		 * 		System.out.println("O cargo foi editado com sucesso."); 
-		 * } catch (SQLException e) {
-		 * 		System.out.println("Ocorreu um erro na alteração do cargo.");
-		 * 		e.printStackTrace(); 
-		 * }
+		 * // TRATAMENTO DE ERRO DE EXCLUSÃO NO BANCO DE DADOS try {
+		 * cdao.excluirCargo(c1); cdao.excluirCargo(c2);
+		 * System.out.println("Os cargos foram excluidos com sucesso."); } catch
+		 * (SQLException e) {
+		 * System.out.println("Ocorreu um erro ao tentar excluir o(s) cargo(s)."
+		 * ); e.printStackTrace(); }
 		 */
 
-//CÓDIGO PARA CONSULTA NO BANCO DE DADOS
+		/*
+		 * CÓDIGO ALTERAÇÃO DOS DADOS NA TABELA Cargo c1 = new Cargo();
+		 * c1.setCodigo(2L); c1.setDescricao("Analista de Expedição");
+		 * 
+		 * CargoDAO cdao = new CargoDAO();
+		 * 
+		 * // TRATAMENTO DE ERRO DA ALTERAÇÃO NO BANCO DE DADOS try {
+		 * cdao.editarCargo(c1);
+		 * System.out.println("O cargo foi editado com sucesso."); } catch
+		 * (SQLException e) {
+		 * System.out.println("Ocorreu um erro na alteração do cargo.");
+		 * e.printStackTrace(); }
+		 */
+
+		/*
+		// CÓDIGO PARA CONSULTA NO BANCO DE DADOS
 		Cargo c1 = new Cargo();
 		c1.setCodigo(1L);
-		
+
 		Cargo c2 = new Cargo();
 		c2.setCodigo(9L);
-		
+
 		CargoDAO cdao = new CargoDAO();
-		
-// TRATAMENTO DE ERRO DA CONSULTA NO BANCO DE DADOS
+
+		// TRATAMENTO DE ERRO DA CONSULTA NO BANCO DE DADOS
 		try {
 			Cargo f3 = cdao.buscarPorCodigo(c1);
 			Cargo f4 = cdao.buscarPorCodigo(c2);
@@ -169,7 +214,35 @@ public class CargoDAO {
 		} catch (SQLException e) {
 			System.out.println("Ocorreu um erro ao tentar perquisar um dos cargos.");
 			e.printStackTrace();
-		}
+		}*/
 		
+		
+		/*
+		// TRATAMENTO DE ERRO E LISTAGEM DA CONSULTA NO BANCO DE DADOS POR CÓDIGO
+		CargoDAO cdao = new CargoDAO();
+		try {
+			ArrayList<Cargo> lista = cdao.listar();
+			
+			for(Cargo c : lista){
+				System.out.println("Resultado: " + c);
+			}
+		}catch(SQLException e) {
+			System.out.println("Ocorreu um erro ao tentar listar os cargos!");
+			e.printStackTrace();
+		} */
+		
+		// TRATAMENTO DE ERRO E LISTAGEM DA CONSULTA NO BANCO DE DADOS POR DESCRIÇÃO
+		Cargo c1 = new Cargo();
+		c1.setDescricao("de");
+		
+		CargoDAO cdao = new CargoDAO();
+		
+		try {
+			ArrayList<Cargo> lista = cdao.buscarPorDescricao(c1);
+		}catch(SQLException e) {
+			System.out.println("Ocorreu um erro ao tentar perquisar um cargo.");
+			e.printStackTrace();
+		}
+
 	}
 }
--- src/br/com/estocandowebjava/dao/SetorDAO.java
+++ src/br/com/estocandowebjava/dao/SetorDAO.java
@@ -1,5 +1,174 @@
 package br.com.estocandowebjava.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.fabric.FabricCommunicationException;
+
+import br.com.estocandowebjava.domain.Setor;
+import br.com.estocandowebjava.factoty.ConexaoFactory;
+
 public class SetorDAO {
+
+	// DEFINIÇÃO DO COMANDO SQL PARA SALVAR OS DADOS
+	public void salvarSetor(Setor s) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("insert into Setor (descricao, andar) values (?, ?) ");
+
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		Connection conexao = ConexaoFactory.conectar();
+
+		// COMANDO DE PREPARAÇÃO
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, s.getDescricao());
+		comando.setString(2, s.getAndar());
+
+		comando.executeUpdate();
+	}
+
+	// DEFINIÇÃO DO COMANDO SQL PARA EXCLUIR DADOS
+	public void excluirSetor(Setor s) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("delete from Setor where cod_setor = ? ");
+
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		Connection conexao = ConexaoFactory.conectar();
+
+		// COMANDO DE PREPARAÇÃO
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, s.getCodigo());
+
+		comando.executeUpdate();
+
+	}
+
+	// DEFINIÇÃO DO COMANDO SQL PARA EDITAR OS DADOS
+	public void editarSetor(Setor s) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("update Setor set descricao = ? where cod_setor = ? ");
+
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		Connection conexao = ConexaoFactory.conectar();
+
+		// COMANDO DE PREPARAÇÃO
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, s.getDescricao());
+		comando.setLong(2, s.getCodigo());
+
+		comando.executeUpdate();
+
+	}
+
+	// DEFINIÇÃO DO COMANDO SQL PARA EDITAR OS DADOS
+	public Setor buscarPorCodigo(Setor s) throws SQLException {
+		StringBuilder sql = new StringBuilder();
+		sql.append("select cod_setor, descricao from Cargo where cod_setor = ? ");
+
+		// CRIAÇÃO DA CONEXÃO COM O BANCO DE DADOS
+		Connection conexao = ConexaoFactory.conectar();
+
+		// COMANDO DE PREPARAÇÃO
+		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setLong(1, s.getCodigo());
+
+		ResultSet resultado = comando.executeQuery();
+
+		Setor retorno = null;
+
+		// CONDIÇÃO QUE DIZ SE A CONSULTA RETORNOU MAIS DE UM VALOR
+		if (resultado.next()) {
+			retorno = new Setor();
+			retorno.setCodigo(resultado.getLong("cod_setor"));
+			retorno.setDescricao(resultado.getString("descricao"));
+		}
+
+		return retorno;
+	}
+
+	// CRIAÇÃO DO MÉTODO MAIN
+	public static void main(String[] args) {
+			
+			/* //CÓDIGO INSERÇÃO DOS DADOS NA TABELA 
+			 Setor s1 = new Setor();
+			 s1.setDescricao("Gestor de Transportes"); 
+			 s1.setAndar("Térreo");
+			 
+			 Setor s2 = new Setor(); 
+			 s2.setDescricao("Analista de Patrimônio");
+			 s2.setAndar("1º Andar");
+			 
+			 SetorDAO sdao = new SetorDAO(); 
+			 // TRATAMENTO DE ERRO DA PERSISTÊNCIA NO BANCO DE DADOS 
+			 try { 
+			 		sdao.salvarSetor(s1); 
+			 		sdao.salvarSetor(s2);
+			 		System.out.println("O(s) setor(es) foi(ram) salvo(s) com sucesso."); 
+			 } catch (SQLException e) {
+			 		System.out.println("Ocorreu um erro ao tentar salvar os dados.");
+			 		e.printStackTrace(); 
+			 } */
+			 
+
+			 
+			 //CÓDIGO INSERÇÃO DOS DADOS NA TABELA 
+			 /*Setor s1 = new Setor();
+			 s1.setCodigo(3L);
+			 
+			 Setor s2 = new Setor(); 
+			 s2.setCodigo(7L);
+			 
+			 SetorDAO sdao = new SetorDAO();
+			 
+			 // TRATAMENTO DE ERRO DE EXCLUSÃO NO BANCO DE DADOS 
+			 try {
+			 		sdao.excluirSetor(s1); 
+			 		sdao.excluirSetor(s2);
+			 		System.out.println("O(s) setor(es) foi(ram) excluido(s) com sucesso."); 
+			 } catch (SQLException e) {
+			 	System.out.println("Ocorreu um erro ao tentar excluir o(s) setor(es)."); 
+			 	e.printStackTrace(); 
+			 } */
+			 
+			 
+			 /*
+			 //CÓDIGO ALTERAÇÃO DOS DADOS NA TABELA 
+			 Setor s1 = new Setor();
+			 s1.setCodigo(2L); 
+			 s1.setDescricao("Andar 1");
+			 
+			 SetorDAO sdao = new SetorDAO(); 
+			 
+			 // TRATAMENTO DE ERRO DA ALTERAÇÃO NO BANCO DE DADOS
+			 try { sdao.editarSetor(s1);
+			 		System.out.println("O setor foi editado com sucesso."); 
+			 } catch (SQLException e) {
+			 		System.out.println("Ocorreu um erro na alteração do setor.");
+			 		e.printStackTrace(); 
+			 }*/
+			 
+
+	//CÓDIGO PARA CONSULTA NO BANCO DE DADOS
+			Setor s1 = new Setor();
+			s1.setCodigo(1L);
+			
+			Setor s2 = new Setor();
+			s2.setCodigo(9L);
+			
+			SetorDAO sdao = new SetorDAO();
+			
+	// TRATAMENTO DE ERRO DA CONSULTA NO BANCO DE DADOS
+			try {
+				Setor f3 = sdao.buscarPorCodigo(s1);
+				Setor f4 = sdao.buscarPorCodigo(s2);
+				System.out.println("Resultado 1: " + f3);
+				System.out.println("Resultado 2: " + f4);
+			} catch (SQLException e) {
+				System.out.println("Ocorreu um erro ao tentar perquisar um dos cargos.");
+				e.printStackTrace();
+			}
+			
+		}
 
 }
