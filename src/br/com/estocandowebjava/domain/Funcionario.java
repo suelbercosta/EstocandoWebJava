efdb77939445ebf99a23052ee64c6b8fd1761f3e
Suelber Costa <suelbercost@gmail.com>
2017-05-18 17:13:02 -0300

    Início da codificação da classe domínio Funcionário

--- src/br/com/estocandowebjava/domain/Funcionario.java
+++ src/br/com/estocandowebjava/domain/Funcionario.java
@@ -1,88 +1,111 @@
 package br.com.estocandowebjava.domain;
 
-import java.util.*;
-
 public class Funcionario {
 	private Long matricula;
+	private String nome;
+	private String cpf;
+	private String rg;
+	private String data_nasc;
+	private String sexo;
 	private Long ctps;
 	private Long pis;
-	private String sexo;
+	private String data_admissao;
 	private String tipo_sang;
-	private Date data_admissao;
-	private Long cargo;
-	private Long setor;
-	private Long endereco;
-
+	private Cargo cargo;
+	private String login;
+	private String senha;
+	private Setor setor;
+	private Endereco endereco;
+	
 	public Long getMatricula() {
 		return matricula;
 	}
-
 	public void setMatricula(Long matricula) {
 		this.matricula = matricula;
 	}
-
-	public Long getCtps() {
-		return ctps;
+	public String getNome() {
+		return nome;
 	}
-
-	public void setCtps(Long ctps) {
-		this.ctps = ctps;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
-
-	public Long getPis() {
-		return pis;
+	public String getCpf() {
+		return cpf;
 	}
-
-	public void setPis(Long pis) {
-		this.pis = pis;
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
 	}
-
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getData_nasc() {
+		return data_nasc;
+	}
+	public void setData_nasc(String data_nasc) {
+		this.data_nasc = data_nasc;
+	}
 	public String getSexo() {
 		return sexo;
 	}
-
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
-
+	public Long getCtps() {
+		return ctps;
+	}
+	public void setCtps(Long ctps) {
+		this.ctps = ctps;
+	}
+	public Long getPis() {
+		return pis;
+	}
+	public void setPis(Long pis) {
+		this.pis = pis;
+	}
+	public String getData_admissao() {
+		return data_admissao;
+	}
+	public void setData_admissao(String data_admissao) {
+		this.data_admissao = data_admissao;
+	}
 	public String getTipo_sang() {
 		return tipo_sang;
 	}
-
 	public void setTipo_sang(String tipo_sang) {
 		this.tipo_sang = tipo_sang;
 	}
-
-	public Date getData_admissao() {
-		return data_admissao;
-	}
-
-	public void setData_admissao(Date data_admissao) {
-		this.data_admissao = data_admissao;
-	}
-
-	public Long getCargo() {
+	public Cargo getCargo() {
 		return cargo;
 	}
-
-	public void setCargo(Long cargo) {
+	public void setCargo(Cargo cargo) {
 		this.cargo = cargo;
 	}
-
-	public Long getSetor() {
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Setor getSetor() {
 		return setor;
 	}
-
-	public void setSetor(Long setor) {
+	public void setSetor(Setor setor) {
 		this.setor = setor;
 	}
-
-	public Long getEndereco() {
+	public Endereco getEndereco() {
 		return endereco;
 	}
-
-	public void setEndereco(Long endereco) {
+	public void setEndereco(Endereco endereco) {
 		this.endereco = endereco;
 	}
-
+	
 }
