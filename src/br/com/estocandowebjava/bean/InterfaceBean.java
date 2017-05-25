d9579ec916322587e58f5477cc1dc75433fb5ea9
Suelber Costa <suelbercost@gmail.com>
2017-05-24 23:12:08 -0300

    Criação da Interface Java.

---
+++ src/br/com/estocandowebjava/bean/InterfaceBean.java
@@ -0,0 +1,16 @@
+package br.com.estocandowebjava.bean;
+
+public interface InterfaceBean {
+	
+	public void carregarListagem();
+	
+	public void prepararNovo();
+	
+	public void novo();
+	
+	public void excluir();
+	
+	public void prepararEditar();
+	
+	public void editar();
+}
