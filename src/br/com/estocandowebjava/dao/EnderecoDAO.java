91dbb5169694d9d2c5a6f953e1c2cff81978c25f
Suelber Costa <suelbercost@gmail.com>
2017-05-26 11:02:06 -0300

    Pequena alteração na classe EnderecoBean

--- src/br/com/estocandowebjava/dao/EnderecoDAO.java
+++ src/br/com/estocandowebjava/dao/EnderecoDAO.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import com.mysql.fabric.FabricCommunicationException;
-
 import br.com.estocandowebjava.domain.Endereco;
 import br.com.estocandowebjava.factoty.ConexaoFactory;
 
