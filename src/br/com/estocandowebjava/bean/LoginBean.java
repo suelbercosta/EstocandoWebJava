14074be6d1072e7dd5fe4c409c3287f17c1dbd95
Suelber Costa <suelbercost@gmail.com>
2017-05-30 20:25:51 -0300

    Criação da classe LoginBean

---
+++ src/br/com/estocandowebjava/bean/LoginBean.java
@@ -0,0 +1,60 @@
+package br.com.estocandowebjava.bean;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import br.com.estocandowebjava.dao.FuncionarioDAO;
+import br.com.estocandowebjava.domain.Cargo;
+import br.com.estocandowebjava.domain.Funcionario;
+
+@ManagedBean(name = "MBLogin")
+@ViewScoped
+public class LoginBean {
+	private Funcionario login;
+	private Funcionario senha;
+	private ArrayList<Funcionario> itens;
+	private ArrayList<Funcionario> itensFiltrados;
+
+	public Funcionario getLogin() {
+		return login;
+	}
+
+	public void setLogin(Funcionario login) {
+		this.login = login;
+	}
+
+	public Funcionario getSenha() {
+		return senha;
+	}
+
+	public void setSenha(Funcionario senha) {
+		this.senha = senha;
+	}
+
+	public ArrayList<Funcionario> getItens() {
+		return itens;
+	}
+
+	public void setItens(ArrayList<Funcionario> itens) {
+		this.itens = itens;
+	}
+
+	public ArrayList<Funcionario> getItensFiltrados() {
+		return itensFiltrados;
+	}
+
+	public void setItensFiltrados(ArrayList<Funcionario> itensFiltrados) {
+		this.itensFiltrados = itensFiltrados;
+	}
+
+	@PostConstruct
+	public void login() {
+			FuncionarioDAO dao = new FuncionarioDAO();
+		//	itens = dao.validarLogin(Funcionario);
+
+	}
+}
