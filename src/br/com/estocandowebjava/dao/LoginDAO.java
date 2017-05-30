d70e846d22fcd348885afa496d6b00690b5adb55
Suelber Costa <suelbercost@gmail.com>
2017-05-30 20:29:06 -0300

    Codificação da classe loginDAO.

--- src/br/com/estocandowebjava/dao/LoginDAO.java
+++ src/br/com/estocandowebjava/dao/LoginDAO.java
@@ -4,38 +4,46 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
-import br.com.estocandowebjava.domain.Cargo;
 import br.com.estocandowebjava.domain.Funcionario;
 import br.com.estocandowebjava.factoty.ConexaoFactory;
 
 public class LoginDAO {
 	
-	public void validarLogin() throws SQLException{
-		Funcionario func = new Funcionario();
-		Cargo cargo = new Cargo();
+	public void validarLogin(Funcionario login, Funcionario senha, Funcionario permissao) throws SQLException{
 		
 		StringBuilder sql = new StringBuilder();
 		sql.append("SELECT f.nome, f.login, f.senha, c.permissao ");
 		sql.append("FROM Funcionario f inner join Cargo c ");
 		sql.append("on c.cod_cargo = f.Cargo_cod_cargo ");
-		sql.append("WHERE c.permissao = 0; ");
+		sql.append("WHERE f.login = ? AND f.senha = ? c.permissao != 0; ");
 		
 		Connection conexao = ConexaoFactory.conectar();
 		
 		PreparedStatement comando = conexao.prepareStatement(sql.toString());
+		comando.setString(1, login.getLogin());
+		comando.setString(2, senha.getSenha());
+		comando.setLong(3, permissao.getCargo().getPermissao());
 		
 		ResultSet resultado = comando.executeQuery();
 		
-		ArrayList<Funcionario> itens = new ArrayList<Funcionario>();
+		String log = resultado.getString("login.login");
+		String sen = resultado.getString("senha.senha");
+		Long perm = resultado.getLong("permissao.permissao");
 		
-		func.setLogin(resultado.getString("func.login"));
-		func.setSenha(resultado.getString("func.senha"));
-		cargo.setPermissao(resultado.getLong("cargo.permissao"));
-		func.setCargo(cargo);
+		if (log == "" && sen == "" ) {
+			System.out.println("Você não é cadastrado para utilizar o sistema, verifique suas permissões.");
+		}
+		else {
+			if (perm == 1) {
+				
+			}
+			if (perm == 2) {
+				
+			}
+		}
 		
-		itens.add(func);
+		
 		
 
 	}
