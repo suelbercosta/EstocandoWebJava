9db0e9cc35560e7eb844c9699d4670375851dce1
Suelber Costa <suelbercost@gmail.com>
2017-06-06 01:34:12 -0300

    Criação e codificação da classe domínio NotaFiscal_Produto.

---
+++ src/br/com/estocandowebjava/domain/NotaFiscal_Produto.java
@@ -0,0 +1,21 @@
+package br.com.estocandowebjava.domain;
+
+public class NotaFiscal_Produto {
+	private NotaFiscal notafiscal;
+	private Produto produto;
+	
+	public NotaFiscal getNotafiscal() {
+		return notafiscal;
+	}
+	public void setNotafiscal(NotaFiscal notafiscal) {
+		this.notafiscal = notafiscal;
+	}
+	public Produto getProduto() {
+		return produto;
+	}
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+	
+	
+}
